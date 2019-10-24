e068caf06172fa68703923b75d6256730af0ced0
William <williambatistapereira@hotmail.com>
2019-10-24 14:39:16 -0300

    pequenas alterações na classe Usuario e no index

--- src/java/Pkt_aula_06/Usuario.java
+++ src/java/Pkt_aula_06/Usuario.java
@@ -8,7 +8,7 @@ package Pkt_aula_06;
 public class Usuario {
            
 
-       static String nome;
+       static String nome;   //static= carrega na memória antes de iniciar o programa é compartilhada... todos recebem o mesmo valor que foi escrito por ultimo.
        static String email;
        static String senha;   
 }
