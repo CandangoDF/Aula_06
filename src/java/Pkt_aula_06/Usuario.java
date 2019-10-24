52edfb370b2ae5bbe6d0fecdd8353f1ef6118c79
William <williambatistapereira@hotmail.com>
2019-10-24 09:59:25 -0300

    Aula 06 do curso java

---
+++ src/java/Pkt_aula_06/Usuario.java
@@ -0,0 +1,16 @@
+
+package Pkt_aula_06;
+
+/**
+ *
+ * @author WillB
+ */
+public class Usuario {
+           
+
+       static String nome;
+       static String email;
+       static String senha;
+}
+
+       
